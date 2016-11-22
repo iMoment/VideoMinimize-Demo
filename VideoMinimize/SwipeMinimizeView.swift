9f9e46f414d29205cac780012244e9e3caffb3e2
Stanley Pan <stanleypan.egre@gmail.com>
2016-11-23 08:09:44 +0900

    Get pan gesture swiping up and down working

--- VideoMinimize/SwipeMinimizeView.swift
+++ VideoMinimize/SwipeMinimizeView.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class SwipeMinimizeView: UIView {
+// MARK: Comparison operators with optionals removed from Swift Standard Libary
+// Copy/pasted from previous projects converted to Swift 3 to save time.
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l < r
+    case (nil, _?):
+        return true
+    default:
+        return false
+    }
+}
+
+// MARK: Comparison operators with optionals removed from Swift Standard Libary
+// Copy/pasted from previous projects converted to Swift 3 to save time.
+fileprivate func >= <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l >= r
+    default:
+        return !(lhs < rhs)
+    }
+}
+
+class SwipeMinimizeView: UIView, UIGestureRecognizerDelegate {
     
+    // Mark: View Properties
     var startingCenter: CGPoint?
     var finalCenter: CGPoint?
     var startingSize: CGSize?
     var finalSize: CGSize?
-    let aspectRatio: CGFloat = 0.5625
+    let aspectRatio: CGFloat = 0.5625 // This is 16:9 in decimal form
     
     var rangeTotal: CGFloat!
     var widthRange: CGFloat!
@@ -23,14 +48,16 @@ class SwipeMinimizeView: UIView {
     // Called when making view programmatically
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.initializeGesture()
     }
     
     // Called when creating view from Storyboard
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        fatalError("init(coder:) has not been implemented")
+        self.initializeGesture()
     }
 
+    // Initialize PanGesture properties
     func initializeGesture() {
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanning))
         panGesture.minimumNumberOfTouches = 1
@@ -38,24 +65,103 @@ class SwipeMinimizeView: UIView {
         self.addGestureRecognizer(panGesture)
     }
     
-    func handlePanning() {
-        
-    }
-    
+    // Configure swipeMinimizeView
     func configureSizeAndPosition(_ parentViewFrame: CGRect) {
         
+        // Set starting and final center of swipeMinimizeView
         self.startingCenter = self.center
-        self.finalCenter = CGPoint(x: parentViewFrame.size.width - parentViewFrame.size.width/4, y: parentViewFrame.size.height - (self.frame.size.height/4) - 2)
+        self.finalCenter = CGPoint(x: parentViewFrame.size.width - parentViewFrame.size.width/4,
+                                   y: parentViewFrame.size.height - self.frame.size.height/4)
         
+        // Set starting and final size of swipeMinimizeView
         startingSize = self.frame.size
-        finalSize = CGSize(width: parentViewFrame.size.width/2 - 10, height: (parentViewFrame.size.width/2 - 10) * aspectRatio)
+        finalSize = CGSize(width: parentViewFrame.size.width/2 - 10,
+                           height: (parentViewFrame.size.width/2) * aspectRatio)
         
         // Set common range totals once
         rangeTotal = finalCenter!.y - startingCenter!.y
         widthRange = startingSize!.width - finalSize!.width
         centerXRange = finalCenter!.x - startingCenter!.x
     }
+    
+    // Handles the panning for swipeMinimizeView
+    func handlePanning(_ panGesture: UIPanGestureRecognizer) {
+        // Get translation point and the state of our pan gesture
+        let translatedPoint = panGesture.translation(in: self.superview!)
+        var gestureState = panGesture.state
+        
+        // Keep track of y-position of our pan gesture
+        // If change less than start, or greater than final center of swipeMinimizeView, end the state
+        let yChange = panGesture.view!.center.y + translatedPoint.y
+        
+        if yChange < startingCenter?.y {
+            gestureState = UIGestureRecognizerState.ended
+        } else if yChange >= finalCenter?.y {
+            gestureState = UIGestureRecognizerState.ended
+        }
+        
+        // Scale if gesture began or changed
+        if gestureState == UIGestureRecognizerState.began || gestureState == UIGestureRecognizerState.changed  {
+            
+            // Modify size as view is panned down
+            let progress = ((panGesture.view!.center.y - startingCenter!.y) / rangeTotal)
+            
+            let invertedProgress = 1 - progress
+            let newWidth = finalSize!.width + (widthRange * invertedProgress)
+            
+            panGesture.view?.frame.size = CGSize(width: newWidth, height: newWidth * aspectRatio)
+            
+            // Makes sure center x value moves along with size change
+            let finalX = startingCenter!.x + (centerXRange * progress)
+            
+            panGesture.view?.center = CGPoint(x: finalX, y: panGesture.view!.center.y + translatedPoint.y)
+            panGesture.setTranslation(CGPoint(x: 0, y: 0), in: self.superview)
+            
+        } else if gestureState == UIGestureRecognizerState.ended {
+            
+            let topDistance = yChange - startingCenter!.y
+            let bottomDistance = finalCenter!.y - yChange
+            
+            var chosenCenter: CGPoint = CGPoint.zero
+            var chosenSize: CGSize = CGSize.zero
+            self.isUserInteractionEnabled = false
+            
+            if topDistance > bottomDistance {
+                // Animate to bottom
+                chosenCenter = finalCenter!
+                chosenSize = finalSize!
+                
+            } else {
+                // Animate to top
+                chosenCenter = startingCenter!
+                chosenSize = startingSize!
+            }
+            
+            // Default to a size and center if center is not starting or final
+            if panGesture.view?.center != chosenCenter {
+                UIView.animate(withDuration: 0.4, animations: {
+                    panGesture.view?.frame.size = chosenSize
+                    panGesture.view?.center = chosenCenter
+                    
+                }, completion: {(done: Bool) in
+                    self.isUserInteractionEnabled = true
+                })
+            } else {
+                self.isUserInteractionEnabled = true
+            }
+        }
+    }
+    
+    // Delegate method to recognize two gestures at the same time, defaulting to NO
+    // TODO: may need to set to YES/true later
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false
+    }
 }
+
+
+
+
 
 
 
--- VideoMinimize/ViewController.swift
+++ VideoMinimize/ViewController.swift
@@ -7,26 +7,49 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var swipeMinimizeView: SwipeMinimizeView!
+    var moviePlayerController: MPMoviePlayerViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create path to mp4, set up properties
+        let path = Bundle.main.path(forResource: "beach", ofType: "mp4")
+        moviePlayerController = MPMoviePlayerViewController(contentURL: URL(fileURLWithPath: path!))
+        moviePlayerController.moviePlayer.controlStyle = MPMovieControlStyle.none
+        moviePlayerController.moviePlayer.scalingMode = MPMovieScalingMode.aspectFit
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        
         swipeMinimizeView.configureSizeAndPosition(self.view.frame)
+        
+        // Make MPMoviePlayerViewController same size as custom swipeMinimizeView, add it as subview
+        moviePlayerController.view.frame = CGRect(x: 0, y: 0, width: swipeMinimizeView.frame.size.width, height: swipeMinimizeView.frame.size.height)
+        swipeMinimizeView.addSubview(moviePlayerController.view)
+        moviePlayerController.moviePlayer.pause()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Reveal custom swipeMinimizeView, play video
+    @IBAction func playMovie(_ sender: UIButton) {
+        self.swipeMinimizeView.isHidden = false
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 0.4, animations: {
+                self.swipeMinimizeView.alpha = 1.0
+            }, completion: {(done: Bool) in
+                self.moviePlayerController.moviePlayer.play()
+            })
+        }
+    }
 }
